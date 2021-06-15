fd12eb016876d5f8cd4536e5ed63d41ae5c9f34a
Tianqi Wu <tianqiwu@TianqideAir2121.attlocal.net>
2021-06-15 11:23:47 -0500

    Single List first part done

--- src/SimpleList/index.js
+++ src/SimpleList/index.js
@@ -1,40 +1,53 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, Platform } from 'react-native'
-import Icon from 'react-native-vector-icons/dist/MaterialIcons'
-import { RippleFeedback, IconToggle } from '@protonapp/react-native-material-ui'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Platform,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+import Icon from "react-native-vector-icons/dist/MaterialIcons";
+import {
+  RippleFeedback,
+  IconToggle,
+} from "@protonapp/react-native-material-ui";
 
 export default class SimpleList extends Component {
   static defaultProps = {
     items: [],
-  }
+  };
   state = {
     fullWidth: 0,
-  }
+    currentQuery: "",
+  };
+
   handleLayout = ({ nativeEvent }) => {
-    const { width } = (nativeEvent && nativeEvent.layout) || {}
-    const { fullWidth: prevWidth } = this.state
+    const { width } = (nativeEvent && nativeEvent.layout) || {};
+    const { fullWidth: prevWidth } = this.state;
 
     if (width !== prevWidth) {
-      this.setState({ fullWidth: width })
+      this.setState({ fullWidth: width });
     }
-  }
+  };
 
   renderHeader() {
-    let { listHeader, background, _fonts } = this.props
+    let { listHeader, background, _fonts } = this.props;
 
     if (!listHeader || !listHeader.header || !listHeader.enabled) {
-      return null
+      return null;
     }
-    let space = 0
+    let space = 0;
     if (background && background.enabled) {
-      space = 10
+      space = 10;
     }
 
-    let headerStyles = [styles.header]
+    let headerStyles = [styles.header];
     if (listHeader.styles) {
-      headerStyles.push(listHeader.styles.header)
+      headerStyles.push(listHeader.styles.header);
     } else if (_fonts) {
-      headerStyles.push({ fontFamily: _fonts.heading })
+      headerStyles.push({ fontFamily: _fonts.heading });
     }
 
     return (
@@ -42,8 +55,27 @@ export default class SimpleList extends Component {
         <Text style={headerStyles}>{listHeader.header}</Text>
         <View style={{ height: space }}></View>
       </>
-    )
+    );
   }
+
+  debounce = (fn, time) => {
+    let timeout;
+  
+    return function() {
+      const functionCall = () => fn.apply(this, arguments);
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    }
+  }
+
+  filterElement = (query) => {
+    let timeout;
+    this.setState({ currentQuery: query });
+
+    clearTimeout(timeout);
+      timeout = setTimeout(functionCall, 300);
+  };
 
   render() {
     let {
@@ -52,9 +84,10 @@ export default class SimpleList extends Component {
       dividerColor,
       background,
       listHeader,
-    } = this.props
+      searchBar,
+    } = this.props;
 
-    let wrap = [styles.wrapper]
+    let wrap = [styles.wrapper];
     if (background && background.enabled) {
       let {
         backgroundColor,
@@ -63,127 +96,188 @@ export default class SimpleList extends Component {
         borderColor,
         rounding,
         shadow,
-      } = background
-      wrap.push({ backgroundColor: backgroundColor, borderRadius: rounding })
+      } = background;
+      wrap.push({ backgroundColor: backgroundColor, borderRadius: rounding });
       if (border) {
-        wrap.push({ borderWidth: borderSize, borderColor: borderColor })
+        wrap.push({ borderWidth: borderSize, borderColor: borderColor });
       }
       if (shadow) {
         wrap.push({
-          shadowColor: '#000000',
+          shadowColor: "#000000",
           shadowOffset: {
             width: 2,
             height: 2,
           },
           shadowOpacity: 0.15,
           shadowRadius: 10,
-        })
+        });
       }
     } else {
       if (listHeader && listHeader.enabled) {
-        wrap.push({ paddingTop: 4 })
+        wrap.push({ paddingTop: 4 });
       } else {
-        wrap.push({ paddingTop: 8 })
+        wrap.push({ paddingTop: 8 });
       }
     }
 
+    const newItems = items.filter(
+      (itm) => itm.firstLine.text.indexOf(this.state.currentQuery) >= 0
+    );
+
     return (
       <>
-        {this.renderHeader()}
+        <SearchBar
+          searchBar={this.props.searchBar}
+          onFilterElement={this.filterElement}
+        ></SearchBar>
+        <>{this.renderHeader()}</>
         <View style={wrap} onLayout={this.handleLayout}>
-          {items.map((itm, i) => (
-            <Row
-              {...itm}
-              key={itm.id}
-              dividerType={dividerType}
-              dividerColor={dividerColor}
-              lastRow={i === items.length - 1}
-              fullWidth={this.state.fullWidth}
-              editor={this.props.editor}
-              _fonts={this.props._fonts}
-            />
-          ))}
+          {newItems.length == 0 ? (
+            <SafeAreaView style={[styles.input], {alignItems: 'center'}}>
+              {searchBar.searchBarNotFoundText}
+            </SafeAreaView>
+          ) : (
+            newItems.map((itm, i) => (
+              <Row
+                {...itm}
+                key={itm.id}
+                dividerType={dividerType}
+                dividerColor={dividerColor}
+                lastRow={i === newItems.length - 1}
+                fullWidth={this.state.fullWidth}
+                editor={this.props.editor}
+                _fonts={this.props._fonts}
+              />
+            ))
+          )}
         </View>
       </>
-    )
+    );
+  }
+}
+
+class SearchBar extends Component {
+  state = {
+    searchResult: "",
+  };
+
+  render() {
+    let { searchBar, onFilterElement } = this.props;
+    if (searchBar.enabled) {
+      return (
+        <>
+          <View
+            style={[
+              styles.searchBar,
+              {
+                backgroundColor: searchBar.backgroundColor,
+                borderWidth: searchBar.borderSize,
+                borderRadius: searchBar.rounding,
+              },
+            ]}
+          >
+            <View style={([styles.icon], {})}>
+              <Icon
+                size={24}
+                name={searchBar.icon}
+                styles={styles.icon}
+                color={searchBar.iconColor}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                style={styles.input}
+                placeholder={searchBar.searchBarPlaceholderText}
+                onChange={(e) => {
+                  this.setState({ searchResult: e.target.value });
+                  onFilterElement(e.target.value);
+                }}
+              />
+            </View>
+          </View>
+        </>
+      );
+    }else {
+      return (<></>)
+    }
   }
 }
 
 class Row extends Component {
   getWidthLimit() {
-    let { leftSection, rightSection, fullWidth } = this.props
-    let leftSectWidth = 0
-    let rightSectWidth = 0
+    let { leftSection, rightSection, fullWidth } = this.props;
+    let leftSectWidth = 0;
+    let rightSectWidth = 0;
 
     if (leftSection && leftSection.enabled) {
-      if (leftSection.type === 'image') {
-        leftSectWidth = 72
+      if (leftSection.type === "image") {
+        leftSectWidth = 72;
       } else {
-        leftSectWidth = 56
+        leftSectWidth = 56;
       }
     }
     if (rightSection && rightSection.enabled) {
-      rightSectWidth = 36
+      rightSectWidth = 36;
     }
-    return fullWidth - leftSectWidth - rightSectWidth - 32
+    return fullWidth - leftSectWidth - rightSectWidth - 32;
   }
 
   getDividerInset() {
-    let { dividerType, leftSection } = this.props
+    let { dividerType, leftSection } = this.props;
 
-    if (dividerType !== 'inset') {
-      return 0
+    if (dividerType !== "inset") {
+      return 0;
     }
 
-    let baseInset = 16
+    let baseInset = 16;
 
     if (!leftSection || !leftSection.enabled) {
-      return baseInset
+      return baseInset;
     }
 
-    if (leftSection.type === 'icon' || leftSection.type === 'avatar') {
-      return baseInset * 2 + 40
+    if (leftSection.type === "icon" || leftSection.type === "avatar") {
+      return baseInset * 2 + 40;
     }
 
-    if (leftSection.type === 'image') {
-      return baseInset * 2 + 56
+    if (leftSection.type === "image") {
+      return baseInset * 2 + 56;
     }
 
-    return 0
+    return 0;
   }
 
   getDividerStyles() {
-    let { dividerColor } = this.props
+    let { dividerColor } = this.props;
 
     if (!dividerColor) {
-      dividerColor = '#e0e0e0'
+      dividerColor = "#e0e0e0";
     }
 
     return {
       left: this.getDividerInset(),
       backgroundColor: dividerColor,
-    }
+    };
   }
 
   hasDivider() {
-    let { dividerType, lastRow } = this.props
+    let { dividerType, lastRow } = this.props;
 
-    if (!lastRow && dividerType && dividerType !== 'none') {
-      return true
+    if (!lastRow && dividerType && dividerType !== "none") {
+      return true;
     }
 
-    return false
+    return false;
   }
 
   renderLeftSection() {
-    let { leftSection, firstLine, secondLine, editor } = this.props
+    let { leftSection, firstLine, secondLine, editor } = this.props;
     if (!leftSection || !leftSection.enabled) {
-      return null
+      return null;
     }
 
-    let source = leftSection.image
+    let source = leftSection.image;
 
-    if (leftSection.type === 'icon') {
+    if (leftSection.type === "icon") {
       //56
       return (
         <View style={styles.iconWrapper} pointerEvents="none">
@@ -193,16 +287,16 @@ class Row extends Component {
             color={leftSection.iconColor}
           />
         </View>
-      )
+      );
     }
 
-    if (leftSection.type === 'avatar') {
-      let avatarStyle = [styles.avatar]
+    if (leftSection.type === "avatar") {
+      let avatarStyle = [styles.avatar];
 
       if (firstLine.titleLineNum > 2 || secondLine.subtitleLineNum > 2) {
-        avatarStyle.push({ marginTop: 18 })
+        avatarStyle.push({ marginTop: 18 });
       } else if (!editor) {
-        avatarStyle.push({ marginTop: 16 })
+        avatarStyle.push({ marginTop: 16 });
       }
       return (
         <View style={styles.imageWrapper}>
@@ -213,14 +307,14 @@ class Row extends Component {
             pointerEvents="none"
           />
         </View>
-      )
+      );
     }
 
-    if (leftSection.type === 'image') {
-      let imageStyle = [styles.image]
+    if (leftSection.type === "image") {
+      let imageStyle = [styles.image];
 
       if (firstLine.titleLineNum > 2 || secondLine.subtitleLineNum > 2) {
-        imageStyle.push({ marginTop: 18 })
+        imageStyle.push({ marginTop: 18 });
       }
       return (
         <View style={styles.imageWrapper}>
@@ -231,23 +325,23 @@ class Row extends Component {
             pointerEvents="none"
           />
         </View>
-      )
+      );
     }
   }
 
   renderRightSection() {
-    let { rightSection, firstLine, secondLine } = this.props
+    let { rightSection, firstLine, secondLine } = this.props;
 
     if (!rightSection || !rightSection.enabled) {
-      return null
+      return null;
     }
 
-    let iconStyles = [{ marginRight: -12 }]
-    let iconWrap = [styles.iconWrap]
+    let iconStyles = [{ marginRight: -12 }];
+    let iconWrap = [styles.iconWrap];
 
     if (rightSection.icon) {
       return (
-        <View style={{ justifyContent: 'flex-start' }}>
+        <View style={{ justifyContent: "flex-start" }}>
           <View style={iconWrap}>
             <IconToggle
               name={rightSection.icon}
@@ -260,27 +354,27 @@ class Row extends Component {
             />
           </View>
         </View>
-      )
+      );
     }
 
-    return null
+    return null;
   }
   renderSubtitle() {
-    let { secondLine } = this.props
-    return secondLine && secondLine.enabled
+    let { secondLine } = this.props;
+    return secondLine && secondLine.enabled;
   }
 
   renderContent() {
-    let { leftSection, firstLine, secondLine, _fonts } = this.props
-    let hasDivider = this.hasDivider()
+    let { leftSection, firstLine, secondLine, _fonts } = this.props;
+    let hasDivider = this.hasDivider();
 
-    let row = [styles.row]
+    let row = [styles.row];
     if (
       (firstLine.titleLineNum <= 2 && secondLine.subtitleLineNum <= 2) ||
       !firstLine.titleLineNum ||
       !secondLine.subtitleLineNum
     ) {
-      row.push({ alignItems: 'center' })
+      row.push({ alignItems: "center" });
     }
 
     return (
@@ -305,11 +399,11 @@ class Row extends Component {
           <View style={[styles.divider, this.getDividerStyles()]} />
         ) : null}
       </View>
-    )
+    );
   }
 
   render() {
-    let { onPress } = this.props
+    let { onPress, firstLine } = this.props;
 
     if (onPress) {
       return (
@@ -318,39 +412,39 @@ class Row extends Component {
             {this.renderContent()}
           </RippleFeedback>
         </View>
-      )
+      );
     }
 
-    return <View style={styles.rowWrapper}>{this.renderContent()}</View>
+    return <View style={styles.rowWrapper}>{this.renderContent()}</View>;
   }
 }
 
 class FirstLine extends Component {
   static defaultProps = {
-    text: '',
-    color: '#212121',
-  }
+    text: "",
+    color: "#212121",
+  };
   render() {
-    let { text, color, titleLineNum, widthLimit, _fonts } = this.props
-    let breakless = text.replace(/(\r\n|\n|\r)/gm, '')
+    let { text, color, titleLineNum, widthLimit, _fonts } = this.props;
+    let breakless = text.replace(/(\r\n|\n|\r)/gm, "");
     //custom fonts
-    let customFontStyles = this.props.styles ? this.props.styles.text : null
+    let customFontStyles = this.props.styles ? this.props.styles.text : null;
     let propStyles = [
       { color: customFontStyles ? customFontStyles.color : color },
-    ]
+    ];
     if (this.props.styles) {
       propStyles.push({
         fontFamily: customFontStyles.fontFamily,
         fontWeight: customFontStyles.fontWeight,
-      })
+      });
     } else if (_fonts) {
-      propStyles.push({ fontFamily: _fonts.body })
+      propStyles.push({ fontFamily: _fonts.body });
     }
-    let titleLimit = widthLimit / 7.7
+    let titleLimit = widthLimit / 7.7;
     if (titleLineNum == 2) {
       if (breakless.length > titleLimit) {
-        const firstLine = breakless.substring(0, titleLimit + 1)
-        const i = firstLine.lastIndexOf(' ')
+        const firstLine = breakless.substring(0, titleLimit + 1);
+        const i = firstLine.lastIndexOf(" ");
         return (
           <View style={styles.titleContainer}>
             <Text style={[styles.firstLine, propStyles]}>
@@ -364,13 +458,13 @@ class FirstLine extends Component {
               {breakless.substring(i + 1)}
             </Text>
           </View>
-        )
+        );
       } else {
         return (
           <View style={styles.titleContainer}>
             <Text style={[styles.firstLine, propStyles]}>{breakless}</Text>
           </View>
-        )
+        );
       }
     }
 
@@ -379,7 +473,7 @@ class FirstLine extends Component {
         <Text style={[styles.firstLine, propStyles]} ellipsizeMode="tail">
           {breakless}
         </Text>
-      )
+      );
     }
 
     return (
@@ -390,36 +484,36 @@ class FirstLine extends Component {
       >
         {breakless}
       </Text>
-    )
+    );
   }
 }
 
 class SecondLine extends Component {
   static defaultProps = {
-    text: '',
-    color: '#757575',
-  }
+    text: "",
+    color: "#757575",
+  };
 
   render() {
-    let { text, color, subtitleLineNum, widthLimit, _fonts } = this.props
-    let customFontStyles = this.props.styles ? this.props.styles.text : null
+    let { text, color, subtitleLineNum, widthLimit, _fonts } = this.props;
+    let customFontStyles = this.props.styles ? this.props.styles.text : null;
     let propStyles = [
       { color: customFontStyles ? customFontStyles.color : color },
-    ]
+    ];
     if (this.props.styles) {
       propStyles.push({
         fontFamily: customFontStyles.fontFamily,
         fontWeight: customFontStyles.fontWeight,
-      })
+      });
     } else if (_fonts) {
-      propStyles.push({ fontFamily: _fonts.body })
+      propStyles.push({ fontFamily: _fonts.body });
     }
-    let subtitleLimit = widthLimit / 7
-    let breakless = text.replace(/(\r\n|\n|\r)/gm, '')
+    let subtitleLimit = widthLimit / 7;
+    let breakless = text.replace(/(\r\n|\n|\r)/gm, "");
     if (subtitleLineNum == 2) {
       if (breakless.length > subtitleLimit) {
-        const firstLine = breakless.substring(0, subtitleLimit + 1)
-        const i = firstLine.lastIndexOf(' ')
+        const firstLine = breakless.substring(0, subtitleLimit + 1);
+        const i = firstLine.lastIndexOf(" ");
         return (
           <View style={styles.titleContainer}>
             <Text style={[styles.secondLine, propStyles]}>
@@ -433,13 +527,13 @@ class SecondLine extends Component {
               {breakless.substring(i + 1)}
             </Text>
           </View>
-        )
+        );
       } else {
         return (
           <View style={styles.titleContainer}>
             <Text style={[styles.secondLine, propStyles]}>{breakless}</Text>
           </View>
-        )
+        );
       }
     }
     if (subtitleLineNum > 2) {
@@ -447,7 +541,7 @@ class SecondLine extends Component {
         <Text style={[styles.secondLine, propStyles]} ellipsizeMode="tail">
           {breakless}
         </Text>
-      )
+      );
     }
 
     return (
@@ -458,7 +552,7 @@ class SecondLine extends Component {
       >
         {breakless}
       </Text>
-    )
+    );
   }
 }
 
@@ -469,16 +563,33 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
     marginLeft: 16,
+  },
+  searchBar: {
+    flexDirection: "row",
+    height: 60,
+    padding: 10,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    margin: 15,
+  },
+  input: {
+    flex: 0.95,
+    height: 40,
+    font: "18px",
+  },
+  icon: {
+    justifyContent: "center",
+    flex: 0.05,
   },
   row: {
     paddingLeft: 16,
     paddingRight: 16,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   divider: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     right: 0,
     height: 1,
@@ -487,8 +598,8 @@ const styles = StyleSheet.create({
     marginRight: 32,
     width: 24,
     height: 24,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
+    flexDirection: "column",
+    justifyContent: "flex-start",
     marginTop: 16,
     marginBottom: 16,
   },
@@ -496,7 +607,7 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
   },
-  iconWrap: { justifyContent: 'center', height: 72 },
+  iconWrap: { justifyContent: "center", height: 72 },
   avatar: {
     marginRight: 16,
     borderRadius: 20,
@@ -504,7 +615,7 @@ const styles = StyleSheet.create({
     width: 40,
     marginTop: 8,
     marginBottom: 8,
-    backgroundColor: '#ccc',
+    backgroundColor: "#ccc",
   },
   image: {
     marginRight: 16,
@@ -512,12 +623,12 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     height: 56,
     width: 56,
-    backgroundColor: '#ccc',
+    backgroundColor: "#ccc",
     //paddingTop: '10%',
   },
   imageWrapper: {
-    height: '100%',
-    justifyContent: 'flex-start',
+    height: "100%",
+    justifyContent: "flex-start",
   },
   main: {
     flex: 1,
@@ -532,7 +643,7 @@ const styles = StyleSheet.create({
     lineHeight: 18,
     marginTop: 2,
     fontSize: 14,
-    maxWidth: '100%',
+    maxWidth: "100%",
   },
   titleContainer: {},
-})
+});
