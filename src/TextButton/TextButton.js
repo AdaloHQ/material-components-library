97bd4292d827477fd77376b5a6850d4528a2e73f
Tianqi Wu <tianqiwu@TianqideAir2121.attlocal.net>
2021-07-01 09:53:56 -0500

    Another Fix

--- src/TextButton/TextButton.js
+++ src/TextButton/TextButton.js
@@ -82,7 +82,7 @@ export default class WrappedTextButton extends Component {
   }
 
   renderSub() {
-    let { icon, action, text, upperCase } = this.props
+    let { icon, action, text, upperCase, container } = this.props
 
     let containerStyles = this.getContainerStyles()
     let iconStyles = this.getTextStyles()
@@ -106,7 +106,7 @@ export default class WrappedTextButton extends Component {
             onPress={action && this.submitAction}
             text={this.state.loading ? '' : text}
             style={{
-              container: containerStyles,
+              container: [containerStyles, container],
               icon: iconStyles,
               text: [textStyles, styles.text],
             }}
