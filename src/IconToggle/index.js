29ccb8bdd373945058b3f0bfce664a29d8e96b9f
Zach Harris <zachbharris94@gmail.com>
2021-08-10 15:35:30 -0500

    remove unused imports

--- src/IconToggle/index.js
+++ src/IconToggle/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { IconToggle as ProtonIconToggle } from '@protonapp/react-native-material-ui'
 
