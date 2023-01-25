4cb81b7956d0de627c4413066b25797a28a99eb3
Scott Massey <scott.massey@wustl.edu>
2023-01-24 23:04:06 -0600

    Manifest.json work done

---
+++ src/NavigationBar/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {
+  Platform,
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Text,
+  ActivityIndicator,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+const NavigationBar = (props) => {
+  return (
+    <View>
+      <Text>Navigation Bar</Text>
+    </View>
+  )
+}
+
+export default NavigationBar
