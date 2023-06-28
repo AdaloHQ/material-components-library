5b9c66de49cc5d4c074dbed6291934ce99ad3fad
Alicia De Broux <adebroux55555@gmail.com>
2023-06-28 15:17:45 -0500

    Keep given imgix props

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,20 +15,36 @@ export const applyImgixParameters = (source, layout, imgixProps = {}) => {
     return source
   }
 
-  const path = uri.split(/[?#]/)[0]
+  const [path, query] = uri.split(/[?#]/)
+
+  const givenParams = query && query.split('&')
+  const queryValues = {}
+
+  if (givenParams) {
+    for (const param of givenParams) {
+      if (param) {
+        const [key, value] = param.split('=')
+        queryValues[key] = value
+      }
+    }
+  }
+
   const params = {
     w: layout.width,
     h: layout.height,
     dpr: Math.max(PixelRatio.get(), 2),
     auto: Platform.OS === 'web' ? 'format,compress' : 'compress',
     ...(typeof imgixProps === 'object' && { ...imgixProps }),
+    ...queryValues,
   }
-  const queryParams = Object.keys(params).map(key => `${key}=${params[key]}`).join('&')
+  const queryParams = Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join('&')
 
   if (typeof source === 'object') {
     return {
       ...source,
-      uri: `${path}?${queryParams}`
+      uri: `${path}?${queryParams}`,
     }
   }
 
